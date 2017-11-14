c713e9123b09198edea48aa036f4c070561f6cb4
Rodrigo Limpias <Rodrigo@MacBook-Pro-de-Rodrigo.local>
2017-11-14 12:29:38 +0100

    TableRecyclerViewAdapter created

---
+++ app/src/main/java/com/example/rodrigo/messas/TableRecyclerViewAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.rodrigo.messas
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.rodrigo.messas.model.Plate
+
+// PLATES PODRÍA SER NULL, POR LO QUE HABRÍA QUE USAR ? ALLÁ DONDE SE USE
+class TableRecyclerViewAdapter(val plates: List<Plate>): RecyclerView.Adapter<TableRecyclerViewAdapter.TableViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): TableViewHolder {
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.content_plate, parent, false)
+        return TableViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TableViewHolder?, position: Int) {
+        holder?.bindPlate(plates[position])
+    }
+
+    override fun getItemCount(): Int = plates.size ?: 0
+
+
+
+    inner class TableViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val plateName = itemView.findViewById<TextView>(R.id.content_plateName)
+        val platePhoto = itemView.findViewById<ImageView>(R.id.content_plateView)
+
+        fun bindPlate(plate: Plate) {
+            val context = platePhoto.context
+
+            plateName.text = plate.name
+            platePhoto.setImageResource(plate.image)
+        }
+    }
+}
--- app/src/main/java/com/example/rodrigo/messas/model/Plate.kt
+++ app/src/main/java/com/example/rodrigo/messas/model/Plate.kt
@@ -2,6 +2,6 @@ package com.example.rodrigo.messas.model
 
 import java.io.Serializable
 
-data class Plate(var name: String, var requests: String?, var allergens: List<Allergen>?, var price: Int?, var image: Int?): Serializable {
-    constructor(name: String) : this(name, null, null, null, null)
+data class Plate(var name: String, var requests: String?, var allergens: List<Allergen>?, var price: Int?, var image: Int): Serializable {
+    constructor(name: String, image: Int) : this(name, null, null, null, image)
 }
