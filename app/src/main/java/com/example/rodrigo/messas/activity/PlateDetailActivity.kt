0edc8504f1ebed54950051b95077983b3d60d3ba
Rodrigo Limpias <Rodrigo@MacBook-Pro-de-Rodrigo.local>
2017-11-15 07:44:49 +0100

    PlateDetailActivity interface half created

---
+++ app/src/main/java/com/example/rodrigo/messas/activity/PlateDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.rodrigo.messas.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rodrigo.messas.R
+
+class PlateDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plate_detail)
+    }
+}
--- app/src/main/java/com/example/rodrigo/messas/activity/TableActivity.kt
+++ app/src/main/java/com/example/rodrigo/messas/activity/TableActivity.kt
@@ -2,9 +2,12 @@ package com.example.rodrigo.messas.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.example.rodrigo.messas.R
 import com.example.rodrigo.messas.TableRecyclerViewAdapter
 import com.example.rodrigo.messas.model.Plate
@@ -29,7 +32,12 @@ class TableActivity : AppCompatActivity() {
         tablePlatesList.layoutManager = GridLayoutManager(this, 2)
         tablePlatesList.itemAnimator = DefaultItemAnimator()
 
-
-
+        findViewById<FloatingActionButton>(R.id.add_plate_button)?.setOnClickListener { v: View ->
+            Snackbar.make(
+                    v,
+                    "Aquí lanzaré la actividad de detalle de plato",
+                    Snackbar.LENGTH_LONG)
+                    .show()
+        }
     }
 }
